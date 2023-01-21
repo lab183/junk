c6b2949d4874b596d49f4fb9884b586636076162
rlcarrca <robertlcarr@gmail.com>
2023-01-20 22:20:25 -0500

    Added more useless stuff (Linked List)

--- pkg.go
+++ pkg.go
@@ -5,6 +5,28 @@ import (
 	"time"
 )
 
+type Node struct {
+	data int
+	next *Node
+}
+
+func (n *Node) AddNode(data int) {
+	newNode := Node{data, nil}
+	iter := n
+	for iter.next != nil {
+		iter = iter.next
+	}
+	iter.next = &newNode
+}
+
+func (n *Node) PrintNode() {
+	iter := n
+	for iter != nil {
+		fmt.Println(iter.data)
+		iter = iter.next
+	}
+}
+
 func init() {
 
 }
